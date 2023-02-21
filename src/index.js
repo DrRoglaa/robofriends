b0396ec14bf6878a36d837b624d87119c85273c0
David-Rok <davidrok.roglic@gmail.com>
2023-02-21 21:14:50 +0100

    deleted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
