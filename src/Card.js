ae6582f3460f02facd8b4f238959efca736775ac
David-Rok <davidrok.roglic@gmail.com>
2023-02-22 21:40:17 +0100

    added link

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const Card = () =>{
+    return(
+        <div>
+            <img src=''></img>
+        </div>
+        <div>
+            <h2>Jane Doe</h2>
+        </div>
+        <div>
+            <p>jane.doe@gmail.com</p>
+        </div>
+    )
+}
