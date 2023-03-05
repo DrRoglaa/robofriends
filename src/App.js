e5849be4bcc72aae73026cb9f68f2d18120e34aa
David Rok Roglič <davidrok@itsroglic.com>
2023-03-05 21:10:03 +0100

    line added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users}))
+}
+
 onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value})
 }
@@ -20,13 +27,23 @@ onSearchChange = (event) => {
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+    if (this.state.robots.length === 0){
+        return (
+        <div className='ts'>
+        <h1 className='f2'>Loading...</h1>
+        </div>
+        )
+    } else {
     return(
         <div className='tc'>
         <h1 className='f2'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
         <CardList robots ={filteredRobots}/>
+        </Scroll>
         </div>
     );
+    }
 }
 }
 
